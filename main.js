5c069c91bb8d9f353c15e03cfd526ab925cd9463
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2014-04-21 14:55:48 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,134 @@
+define(function(require, exports, module) {
+	var fileInfo = {
+		html: {
+			color: '#E34C26',
+			icon: '\uf271'
+		},
+		svg: {
+			color: '#ff9900',
+			icon: '\uf271'
+		},
+		xml: {
+			color: '#ff6600',
+			icon: '\uf271'
+		},
+
+		css: {
+			color: '#0270b9',
+			icon: '\uf219'
+		},
+
+		js: {
+			color: '#e5a228',
+			icon: '\uf2db'
+		},
+		ts: {
+			color: '#0074c1',
+			icon: '\uf2db'
+		},
+		coffee: {
+			color: '#28334b',
+			icon: '\uf272'
+		},
+
+		json: {
+			color: '#e5a228',
+			icon: '\uf195'
+		},
+
+		sh: {
+			color: '#008d00',
+			icon: '\uf12e'
+		},
+
+		// images
+		png: {
+			color: '#ff4000',
+			icon: '\uf147'
+		},
+		jpg: {
+			color: '#ff4000',
+			icon: '\uf147'
+		},
+		jpeg: {
+			color: '#ff4000',
+			icon: '\uf147'
+		},
+		tiff: {
+			color: '#ff4000',
+			icon: '\uf147'
+		},
+		gif: {
+			color: '#ff4000',
+			icon: '\uf148'
+		},
+
+		// videos
+		mp4: {
+			color: '#008d00',
+			icon: '\f1f3'
+		},
+		webm: {
+			color: '#008d00',
+			icon: '\f1f3'
+		},
+		ogg: {
+			color: '#008d00',
+			icon: '\f1f3'
+		},
+
+		// audio
+		mp3: {
+			color: '#921100',
+			icon: '\uf259'
+		},
+		wav: {
+			color: '#921100',
+			icon: '\uf259'
+		}
+	};
+	var def = {
+		color: '#fff',
+		icon: '\uf12f'
+	};
+
+	var ProjectManager = brackets.getModule('project/ProjectManager');
+	var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+
+	ExtensionUtils.loadStyleSheet(module, "styles/style.css");
+
+	function renderFiles() {
+		$('#project-files-container ul').removeClass('jstree-no-icons').addClass('jstree-icons');
+
+		$items = $('#project-files-container li>a');
+
+		$items.each(function(index) {
+			var ext = ($(this).find('.extension').text() || '').substr(1);
+
+			var data;
+
+			if ($(this).parent().hasClass('jstree-leaf')) {
+				data = fileInfo[ext] || def;
+			} else {
+				return;
+			}
+
+			$new = $(this).find('.jstree-icon');
+			$new.text(data.icon);
+			$new.addClass('file-icon');
+			$new.css({
+				color: data.color
+			});
+			$(this).prepend($new);
+		});
+	}
+
+	$(ProjectManager).on('projectOpen projectRefresh', function() {
+		var events = 'load_node.jstree create_node.jstree set_text.jstree';
+
+		renderFiles();
+
+		$('#project-files-container').off(events, renderFiles);
+		$('#project-files-container').on(events, renderFiles);
+	});
+});
---
+++ readme.md
@@ -0,0 +1,22 @@
+Brackets Icons
+==============
+This extension adds file icons (from the [Ionicons](https://github.com/driftyco/ionicons) project) to the brackets file tree. The following files are supported at the moment:
+
+ - JavaScript
+ - JSON
+ - TypeScript
+ - CoffeeScript
+ - HTML
+ - SVG
+ - XML
+ - Shell script
+ - PNG, JPG, JPEG, TIFF
+ - GIF
+ - MP4, WebM, OGG
+ - MP3, WAV
+
+You can request more file formats by creating an issue. Choose the icon [here](http://ionicons.com) and add a color (in hex format) to the issue.
+
+License
+-------
+Brackets Icons is licensed under the [MIT license](http://opensource.org/licenses/MIT). Ionicons is also licensed under the MIT license.

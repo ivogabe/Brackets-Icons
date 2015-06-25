3f56f27d37ff4556865730e8ce778d21d7eedce5
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-06-25 21:53:00 +0200

    Add more icons

--- lib/presets.ts
+++ lib/presets.ts
@@ -110,7 +110,7 @@ export function setExtensions(set: (extension: string | string[], icon: PresetIc
 		[IconSet.IconIon]: 'ion-ios-star',
 		[IconSet.IconDev]: 'devicons devicons-groovy'
 	});
-	set('groovy', {
+	set('mf', {
 		[IconSet.ColorLight]: '#2f5796',
 		[IconSet.IconIon]: 'ion-ios-gear'
 	});
@@ -209,7 +209,242 @@ export function setExtensions(set: (extension: string | string[], icon: PresetIc
 		[IconSet.IconIon]: 'ion-social-windows'
 	});
 	
-	// TODO: More icons...
+	// Templating
+	set('jade', {
+		[IconSet.ColorLight]: '#00a57a',
+		[IconSet.IconIon]: 'ion-egg'
+	});
+
+	// Images
+	set('png', {
+		[IconSet.ColorLight]: '#dbb1a9',
+		[IconSet.IconIon]: 'ion-image'
+	});
+	set(['jpeg', 'jpg'], {
+		[IconSet.ColorLight]: '#dedfa3',
+		[IconSet.IconIon]: 'ion-image'
+	});
+	set('tiff', {
+		[IconSet.ColorLight]: '#ff4000',
+		[IconSet.IconIon]: 'ion-image'
+	});
+	set('ico', {
+		[IconSet.ColorLight]: '#b6d2d1',
+		[IconSet.IconIon]: 'ion-image'
+	});
+	set('svg', {
+		[IconSet.ColorLight]: '#c0c5eb',
+		[IconSet.IconIon]: 'ion-image'
+	});
+	set('gif', {
+		[IconSet.ColorLight]: '#aaecc0',
+		[IconSet.IconIon]: 'ion-images'
+	});
+
+	// Videos
+	set(['mp4', 'webm', 'ogg'], {
+		[IconSet.ColorLight]: '#008d00',
+		[IconSet.IconIon]: 'ion-ios-videocam'
+	});
+
+	// Audio
+	set(['mp3', 'wav'], {
+		[IconSet.ColorLight]: '#921100',
+		[IconSet.IconIon]: 'ion-volume-medium'
+	});
+
+	// Fonts
+	set('ttf', {
+		[IconSet.ColorLight]: '#b42950',
+		[IconSet.IconIon]: 'ion-social-tumblr'
+	});
+	set('eot', {
+		[IconSet.ColorLight]: '#b36908',
+		[IconSet.IconIon]: 'ion-social-tumblr'
+	});
+	set(['woff', 'woff2'], {
+		[IconSet.ColorLight]: '#7f4bb2',
+		[IconSet.IconIon]: 'ion-social-tumblr'
+	});
+	set('otf', {
+		[IconSet.ColorLight]: '#7f4bb2',
+		[IconSet.IconIon]: 'ion-social-tumblr'
+	});
+
+	// Readme
+	set(['md', 'markdown'], {
+		[IconSet.ColorLight]: '#709ead',
+		[IconSet.IconIon]: ['ion-social-markdown', 12]
+	});
+
+	// Git
+	set('gitignore', {
+		[IconSet.ColorLight]: '#cd5439',
+		[IconSet.IconIon]: ['ion-minus-circled', 14]
+	});
+	set('gitmodules', {
+		[IconSet.ColorLight]: '#f64d27',
+		[IconSet.IconIon]: ['ion-fork-repo', 17]
+	});
+
+	// Webservers
+	set('htaccess', {
+		[IconSet.ColorLight]: '#93a8be',
+		[IconSet.IconIon]: ['ion-ios-unlocked', 18]
+	});
+	set('htpasswd', {
+		[IconSet.ColorLight]: '#6c369c',
+		[IconSet.IconIon]: ['ion-ios-locked', 18]
+	});
+	set('conf', {
+		[IconSet.ColorLight]: '#009900',
+		[IconSet.IconIon]: 'ion-ios-gear'
+	});
+
+	// Archive
+	set('zip', {
+		[IconSet.ColorLight]: '#008858',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('rar', {
+		[IconSet.ColorLight]: '#005888',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('7z', {
+		[IconSet.ColorLight]: '#880058',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('tgz', {
+		[IconSet.ColorLight]: '#7900bc',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('tar', {
+		[IconSet.ColorLight]: '#885800',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('gz', {
+		[IconSet.ColorLight]: '#588800',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('bzip', {
+		[IconSet.ColorLight]: '#884300',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('msi', {
+		[IconSet.ColorLight]: '#6f8696',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	set('dmg', {
+		[IconSet.ColorLight]: '#6f8696',
+		[IconSet.IconIon]: 'ion-briefcase'
+	});
+	
+	// Settings
+	set([
+		'project',
+		'jscsrc',
+		'jshintrc',
+		'csslintrc',
+		'htmlhintrc',
+		'xmlhintrc',
+		'todo',
+		'classpath',
+		'properties',
+		'bowerrc',
+		'gruntrc',
+		'jsrc',
+		'pro',
+		'editorconfig'
+	], {
+		[IconSet.ColorLight]: '#777777',
+		[IconSet.IconIon]: 'ion-ios-gear'
+	});
+	set('csproj', {
+		[IconSet.ColorLight]: '#5bb552',
+		[IconSet.IconIon]: ['ion-ios-paper-outline', 18]
+	});
+	set('vbproj', {
+		[IconSet.ColorLight]: '#486dae',
+		[IconSet.IconIon]: ['ion-ios-paper-outline', 18]
+	});
+	set('sln', {
+		[IconSet.ColorLight]: '#87c5de',
+		[IconSet.IconIon]: ['ion-ios-paper-outline', 18]
+	});
+
+	// Other text files
+	set('txt', {
+		[IconSet.ColorLight]: '#4192c1',
+		[IconSet.IconIon]: 'ion-document-text'
+	});
+	set('log', {
+		[IconSet.ColorLight]: '#225dc9',
+		[IconSet.IconIon]: 'ion-clipboard'
+	});
+	set('npmignore', {
+		[IconSet.ColorLight]: '#cb3837',
+		[IconSet.IconIon]: ['ion-minus-circled', 14]
+	});
+	set('slugignore', {
+		[IconSet.ColorLight]: '#0da064',
+		[IconSet.IconIon]: ['ion-minus-circled', 14]
+	});
+	set(['yml', 'yaml'], {
+		[IconSet.ColorLight]: '#008000',
+		[IconSet.IconIon]: ['ion-navicon', 14]
+	});
+	set('sqf', {
+		[IconSet.ColorLight]: '#b9e11f',
+		[IconSet.IconIon]: 'ion-wand'
+	});
+	
+	// LaTeX
+	set(['tex', 'bib', 'sty'], {
+		[IconSet.ColorLight]: '#262686',
+		[IconSet.IconIon]: 'ion-document-text'
+	});
+
+	//Singular Types
+	set('applescript', {
+		[IconSet.ColorLight]: '#afafaf',
+		[IconSet.IconIon]: 'ion-social-apple'
+	});
+	set('textile', {
+		[IconSet.ColorLight]: '#6f8696',
+		[IconSet.IconIon]: 'ion-quote'
+	});
+	set('matlab', {
+		[IconSet.ColorLight]: '#014495',
+		[IconSet.IconIon]: 'ion-clipboard'
+	});
+	set('lisp', {
+		[IconSet.ColorLight]: '#f8c63d',
+		[IconSet.IconIon]: 'ion-ios-paperplane'
+	});
+	set('xsl', {
+		[IconSet.ColorLight]: '#68217a',
+		[IconSet.IconIon]: 'ion-code'
+	});
+	set('tcl', {
+		[IconSet.ColorLight]: '#c3b15f',
+		[IconSet.IconIon]: 'ion-code'
+	});
+	set('rst', {
+		[IconSet.ColorLight]: '#6f8696',
+		[IconSet.IconIon]: 'ion-ios-paper'
+	});
+	set('d', {
+		[IconSet.ColorLight]: '#960000',
+		[IconSet.IconIon]: 'ion-contrast'
+	});
+	set('r', {
+		[IconSet.ColorLight]: '#8495C0',
+		[IconSet.IconIon]: 'ion-ios-analytics'
+	});
+	set('map', {
+		[IconSet.ColorLight]: '#e0591f',
+		[IconSet.IconIon]: 'ion-ios-photos-outline'
+	});
 }
 
 export function getDefault(): PresetIcon {

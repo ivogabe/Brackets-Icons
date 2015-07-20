a09e7db1ee35f1057c68e006b62ee0c12d547be6
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-07-20 21:24:27 +0200

    Add a gulp task to create .zip file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 var gulp = require('gulp');
 var typescript = require('gulp-typescript');
+var zip = require('gulp-zip');
 
 var tsProject = typescript.createProject('lib/tsconfig.json', { typescript: require('typescript') });
 
-gulp.task('default', function() {
+gulp.task('scripts', function() {
 	return tsProject.src()
 		.pipe(typescript(tsProject))
 		.pipe(gulp.dest('dist'));
 });
+
+gulp.task('archive', function() {
+	return gulp.src([
+			'dist/**/*',
+			'fonts/**/*',
+			'*.md',
+			'package.json',
+			'main.js',
+			'styles/**/*'
+		], { base: './' })
+		.pipe(zip('Brackets-Icons.zip'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['zip']);

dd4bdbee48dc08c5b2629857502a45047e5ea1a5
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-12-18 09:38:27 +0100

    Fix indentation

--- lib/presets.ts
+++ lib/presets.ts
@@ -58,7 +58,7 @@ export function setExtensions(set: RegisterIcon) {
 		[IconSet.IconIon]: ['file-icon-c', 13],
 		[IconSet.IconDev]: 'devicons devicons-javascript'
 	});
-    set('jsx', {
+	set('jsx', {
 		[IconSet.ColorLight]: '#f8c63d',
 		[IconSet.IconIon]: ['file-icon-c', 13],
 		[IconSet.IconDev]: 'devicons devicons-javascript'
